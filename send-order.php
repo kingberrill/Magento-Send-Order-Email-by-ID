8b2cd968c1dd75490956c0716767d2a7ad43a727
kingberrill <mail@steve-berrill.com>
2017-08-26 14:17:53 +0100

    send-order.php
    1. add the file to your magento root directory
    2. specify order id
    3. specify email address
    4. run file on directory

---
+++ send-order.php
@@ -0,0 +1,28 @@
+<?php
+// gain access to magento
+error_reporting( E_ALL );
+require_once "app/Mage.php";
+Mage::app('admin');
+
+
+// set order id
+$orderId = '10000000';
+$customEmail = 'your-email';
+
+try {
+	// send order email
+	$order = Mage::getModel('sales/order')->loadByIncrementId($orderId);
+	// check if oirder id exists
+	if ($order->getId()) {
+		$order->setCustomerEmail($customEmail);
+		$order->sendNewOrderEmail();
+		echo "Order #$orderId successfully sent to $customEmail";
+		//print_r($order->debug());
+	} else {
+		// if no order id was found
+		echo "Order #$orderId not found\n";
+	}
+
+} catch (Exception $e) {
+	echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
